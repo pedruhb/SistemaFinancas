70538a1753149fd8475aa15a91b222a7f00944c7
Pedro Henrique <34753501+pedruhb@users.noreply.github.com>
2021-10-27 07:31:45 -0300

    Remover variável inútil

--- api.php
+++ api.php
@@ -10,8 +10,6 @@ if (!isset($_GET["method"])) {
     die(json_encode(array("success" => false, "message" => "Missing Method")));
 }
 
-$postdata = $_POST;
-
 switch ($_GET["type"]) {
 
     case "auth":
